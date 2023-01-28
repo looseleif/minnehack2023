2c90967d6a8f6e30221135297907d309c5af2ca2
Nathan Rowe <nathandrowe1@gmail.com>
2023-01-28 16:03:16 -0600

    Initial commit
    Generated by create-expo-app 1.1.4.

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>our minnehack2023 project</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
