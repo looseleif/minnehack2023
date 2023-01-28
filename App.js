1fa3bc368b646c91fb9be75489d062f91fdd7661
Nathan Rowe <nathandrowe1@gmail.com>
2023-01-28 15:47:48 -0600

    fresh react-native project

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>minnehack2023 pokemon go to the polls project!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,12 @@
+# minnehack2023
+
+how to run?
+
+clone this repo
+download node.js
+in the project terminal run `npm install`
+then run `npx expo start`
+download expo go on mobile device
+make sure connection between the server and the mobile device are the same
+scan the QR code from terminal and open it in expo go
+boom communications
