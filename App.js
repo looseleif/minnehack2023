05d0fee2bd9b46b7a152c0d83f24c147294fce7e
k1llerb333 <akishawy7@gmail.com>
2023-01-28 19:55:12 -0600

    main menu fix

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './screens/MainScreen';
 import { Camera } from 'expo-camera';
-import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 const Stack = createStackNavigator();
 
@@ -26,60 +25,10 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <View> style={styles.Start}
-      <Text style={[styles.start, styles.buttonText]}>Start</Text>
-      <Text style={[styles.options, styles.buttonText]}>options</Text>
-
-      </View>   
-     
-
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Main" component={MainScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-const styles = StyleSheet.create({
-  Start: 
-  {
-    position: 'absolute',
-    top: 0,
-    middle: 1,
-    bottom: 0,
-    left: 0,
-    right: 0,
-   //alignItems: 'center',
-    color: "blue",
-    
-    opacity: 1.0,
-    //width: '100%',
-    //height: '10%',
-    backgroundColor: "red",
-    borderTopColor: 'rgba(0, 0, 0, 0.3)',
-  },
-  options: 
-  {
-    position: 'absolute',
-    top: 0,
-    middle: 1,
-    bottom: 0,
-    left: 0,
-    right: 0,
-   //alignItems: 'center',
-    color: "blue",
-    
-    opacity: 1.0,
-    //width: '100%',
-    height: '95%',
-    backgroundColor: "red",
-    borderTopColor: 'rgba(0, 0, 0, 0.3)',
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: '400',
-    fontSize: 16,
-    
-},
 
-
-})
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { Camera, CameraType } from 'expo-camera';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
     return (
       <View style={styles.container}>
+      <Text style={[styles.start, styles.buttonText]}>Start</Text>
+      <Text style={[styles.options, styles.buttonText]}>options</Text>
         <Camera 
           style={{flex:1}}
           type={CameraType.back}
@@ -15,10 +18,51 @@ const HomeScreen = () => {
   
   export default HomeScreen;
 
-
-const styles = StyleSheet.create({
-    // container style for the main view
-    container: {
-        flex:1
+  const styles = StyleSheet.create({
+        // container style for the main view
+        container: {
+          flex:1
+      },
+    Start: 
+    {
+      position: 'absolute',
+      top: 0,
+      middle: 1,
+      bottom: 0,
+      left: 0,
+      right: 0,
+     //alignItems: 'center',
+      color: "blue",
+      
+      opacity: 1.0,
+      //width: '100%',
+      //height: '10%',
+      backgroundColor: "red",
+      borderTopColor: 'rgba(0, 0, 0, 0.3)',
     },
-});
+    options: 
+    {
+      position: 'absolute',
+      top: 0,
+      middle: 1,
+      bottom: 0,
+      left: 0,
+      right: 0,
+     //alignItems: 'center',
+      color: "blue",
+      
+      opacity: 1.0,
+      //width: '100%',
+      height: '95%',
+      backgroundColor: "red",
+      borderTopColor: 'rgba(0, 0, 0, 0.3)',
+    },
+    buttonText: {
+      color: 'white',
+      fontWeight: '400',
+      fontSize: 16,
+      
+  },
+  
+  
+  })
